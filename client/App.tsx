9c19189ad507c335874200aae007e7ce96d47e9a
jkucharewicz3 <jkucharewicz3@gatech.edu>
2023-10-20 13:23:00 -0400

    Added basic logout functionality

--- client/App.tsx
+++ client/App.tsx
@@ -10,17 +10,20 @@ import {SignUp} from './screens/SignUp';
 import {SignIn} from './screens/SignIn';
 import {ForgotPassword} from './screens/ForgotPassword';
 import {ForgotPasswordEnter} from './screens/ForgotPasswordEnter';
+import {Profile} from  './screens/Profile';
 
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {NavigationContainer} from '@react-navigation/native';
 import { Accounts } from './screens/Accounts';
 import {AccountInfo} from './screens/AccountInfo';
+import FooterList from './components/FooterList';
+import {navigationRef} from './components/RootNavigation';
 
 const Stack = createNativeStackNavigator();
 
 function App(): JSX.Element {
   return (
-    <NavigationContainer>
+    <NavigationContainer ref={navigationRef}>
         <Stack.Navigator initialRouteName="SignIn">
           <Stack.Screen
             name="SignUp"
@@ -67,6 +70,17 @@ function App(): JSX.Element {
             component={AccountInfo}
             options={() => ({
               title: 'Account Info',
+              headerBackVisible: true,
+            })}
+          />
+          
+          <Stack.Screen name="Footer" component={FooterList}/>
+
+          <Stack.Screen
+            name="Profile"
+            component={Profile}
+            options={() => ({
+              title: 'Profile',
               headerBackVisible: true,
             })}
           />
