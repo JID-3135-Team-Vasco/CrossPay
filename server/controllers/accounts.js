bc97a46781dc327a524c0e5084f45c6eb43505f1
Jason Jiang <jasonjiang09@gmail.com>
2023-11-13 19:41:47 -0500

    email notif for added bank accounts

--- server/controllers/accounts.js
+++ server/controllers/accounts.js
@@ -19,7 +19,35 @@ export const getAccounts = async (req, res) => {
   }
 };
 
-//Potentially add notifications when accounts added
+//Email notifications when accounts are added
+export const sendAccountNotif = async(req, res) => {
+  try {
+    const sgMail = require('@sendgrid/mail')
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
+    const { email, accounts } = req.body;
+    console.log("Sending notif to email: ", email);
+    console.log("Sending notif for following accounts: ", accounts)
+
+    const msg = {
+      to: email,
+      from: 'jasonjiang09@gmail.com',
+      subject: 'CrossPay Updated Account',
+      html: '<strong>Hello there! Your bank account has been successfuly added!</strong>',
+    }
+    sgMail
+      .send(msg)
+      .then(() => {
+        console.log('Email sent')
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+    return res.json({ ok: true });
+  } catch (err) {
+    console.log(err);
+  } 
+}
 
 export const updateAccounts = async (req, res) => {
   try {
--- server/routes/accounts.js
+++ server/routes/accounts.js
@@ -3,10 +3,11 @@ import express from "express";
 const router = express.Router();
 
 import {
-  updateAccounts, getAccounts
+  updateAccounts, getAccounts, sendAccountNotif
 } from "../controllers/accounts";
 
 router.post("/update-accounts", updateAccounts);
 router.get("/get-accounts", getAccounts);
+router.post("/send-account-notif", sendAccountNotif);
 
 export default router;
