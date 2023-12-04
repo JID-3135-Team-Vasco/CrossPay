a9bef4368a8bf0b377e032ebdb58580f9b84b6b8
jkucharewicz3 <jkucharewicz3@gatech.edu>
2023-12-03 19:19:07 -0500

    Changed UI color

--- client/screens/Colors.ts
+++ client/screens/Colors.ts
@@ -1,7 +1,7 @@
 export const COLORS = {
   primary: '#00684A',
   secondary: '#89CFF0',
-  lightGreen: '#8dc2b2',
+  lightGreen: '#8dc2b0',
   tertiary: '#1255F0',
   red: '#c52233',
   darkGreen: '#013220',
